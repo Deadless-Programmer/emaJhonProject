60c8b48269c9c3de7bc342e043e529a9cef9b5a6
Deadless-Programmer <webtuhin@gmail.com>
2023-03-28 05:20:06 +0600

    first commit

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Product.css";
+const Product = (props) => {
+  const { img, name, price, ratings, seller } = props.product;
+
+  const addTocartItem = props.addTocartItem;
+
+  return (
+    <div className="product">
+      <img src={img} alt="" />
+      <div className="product-info">
+        <h6 className="title-name">{name}</h6>
+        <p>Price: ${price}</p>
+        <p>Manufacturer : {seller} </p>
+        <p>Rating : {ratings} star </p>
+      </div>
+      <button onClick={()=>addTocartItem(props.product)} className="btn-cart">Add to cart</button>
+    </div>
+  );
+};
+
+export default Product;
